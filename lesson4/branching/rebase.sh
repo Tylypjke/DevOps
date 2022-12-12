e4b24dcd670717dc8a4460691893e2cd95000970
Aleksey L <hzl@mail.ru>
2022-12-12 22:54:08 +0300

    git reset merge sry

--- lesson4/branching/rebase.sh
+++ lesson4/branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< .merge_file_p2NC2O
     echo "Next parameter: $param"
+=======
+    echo "\$@ Parameter #$count = $param"
+>>>>>>> .merge_file_rYBJQk
     count=$(( $count + 1 ))
 done
 
